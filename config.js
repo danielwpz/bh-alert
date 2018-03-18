8bcd59f4349100b1f1b226a4c5d1393ad727c506
Daniel Wang <danielwpz@gmail.com>
2018-03-17 17:45:27 -0700

    Add lexar sd card

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ module.exports = {
   bh_base_url: 'https://www.bhphotovideo.com/c/product',
   products: [
     '1082907-REG', // 35 F1.4 silver
-    '1312543-REG' // M10 silver
+    '1312543-REG', // M10 silver
+    '1245340-REG' // Lexar 32GB UHS-1
   ],
   sms: [
     '16507722085'
